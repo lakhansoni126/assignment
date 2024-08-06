a5fef5b1474ee15d8e879fe8e9cad6568450bead
Lakhan <lakhansoni126@gmail.com>
2024-08-06 16:56:03 +0530

    cors error1

--- server/server.js
+++ server/server.js
@@ -7,9 +7,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://assignment-frontend-blush.vercel.app/'
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/orders', orderRoutes);
